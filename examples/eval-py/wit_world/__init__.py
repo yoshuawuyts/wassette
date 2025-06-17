1637295cd4f4ef812a0585a6323a0b52bed67507
Jiaxiao Zhou <duibao55328@gmail.com>
2025-06-16 19:13:57 -0700

    examples: add eval-py example written in Python using componentize-py
    Signed-off-by: Jiaxiao Zhou <duibao55328@gmail.com>

---
+++ examples/eval-py/wit_world/__init__.py
@@ -0,0 +1,27 @@
+from typing import TypeVar, Generic, Union, Optional, Protocol, Tuple, List, Any, Self
+from types import TracebackType
+from enum import Flag, Enum, auto
+from dataclasses import dataclass
+from abc import abstractmethod
+import weakref
+
+from .types import Result, Ok, Err, Some
+
+
+
+class WitWorld(Protocol):
+
+    @abstractmethod
+    def eval(self, expression: str) -> str:
+        """
+        Raises: `wit_world.types.Err(wit_world.imports.str)`
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def exec(self, statements: str) -> str:
+        """
+        Raises: `wit_world.types.Err(wit_world.imports.str)`
+        """
+        raise NotImplementedError
+
