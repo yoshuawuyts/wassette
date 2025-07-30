650a1855b61b8fd990273b7c1deaa230964d414e
Jiaxiao Zhou <duibao55328@gmail.com>
2025-07-30 11:35:58 -0700

    fix(weather): Improve error handling and update API key check (#36)
    Signed-off-by: Jiaxiao Zhou <duibao55328@gmail.com>

--- examples/get-weather-js/weather.js
+++ examples/get-weather-js/weather.js
@@ -1,31 +1,32 @@
 import { get } from "wasi:config/store@0.2.0-draft";
 
 export async function getWeather(city) {
+  const apiKey = await get("OPENWEATHER_API_KEY");
+  if (apiKey === undefined) {
+    throw "Error: OPENWEATHER_API_KEY is not set";
+  }
+
   try {
-    const apiKey = await get("OPENWEATHER_API_KEY");
-    if (apiKey === undefined) {
-      return "Error: OPENWEATHER_API_KEY is not set";
-    }
     const geoResponse = await fetch(
       `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
     );
     if (!geoResponse.ok) {
-      return "Error: Failed to fetch geo data";
+      throw "Error: Failed to fetch geo data";
     }
     const geoData = await geoResponse.json();
     const lat = geoData[0].lat;
     const lon = geoData[0].lon;
-
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-    )
+    );
     if (!response.ok) {
-      return "Error: Failed to fetch weather data";
+      throw "Error: Failed to fetch weather data";
     }
     const data = await response.json();
     const weather = data.main.temp.toString();
-    return weather;
+    return weather;  
   } catch (error) {
-    return "Error fetching weather data";
+    throw error.message || "Error fetching weather data";
   }
+  
 }
